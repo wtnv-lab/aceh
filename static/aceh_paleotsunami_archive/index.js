8257aa4e80188c42b89591a094ef41ce98cfe84c
rot1024 <rot1024@gmail.com>
2017-06-29 19:53:10 +0900

    fix

--- static/aceh_paleotsunami_archive/index.js
+++ static/aceh_paleotsunami_archive/index.js
@@ -7,121 +7,16 @@
         name: "history",
         children: [
           {
-            name: "History",
-            entities: ["Hsitory"]
+            name: "Museum Ali Hasjmy",
+            entities: ["Banda Aceh-Museum Ali Hasjmy"]
           },
           {
             name: "List of Manuscripts related earthquake and tsunami Aceh",
-            content: '\
-              <table>\
-                <thead>\
-                  <tr>\
-                    <th rowspan="2" scope="col">Sources</th>\
-                    <th colspan="3" scope="colgroup">Aceh Paleotsunami historical records</th>\
-                  </tr>\
-                  <tr>\
-                    <th scope="col">Documents</th>\
-                    <th scope="col">Time</th>\
-                    <th scope="col">Contents</th>\
-                  </tr>\
-                </thead>\
-                <tbody>\
-                  <tr>\
-                    <td>Hermansyah: Aceh National Museum</td>\
-                    <td>Earthquake & Eclipse manuscripts 07_00523</td>\
-                    <td>Wednesday, 31th January 1906 M (5th Zulhijah 1324 H)<br />Wa-Shahibul Kitab Ibrahim Lambunot Mukim Lam Krak 1324 H (1906 M)</td>\
-                    <td>“Rajab; If earthquake moves in dhuha, it’s sign that seawater was very hard and very much rainy.”</td>\
-                  </tr>\
-                  <tr>\
-                    <td>Hermansyah: Ali Hasjmy Foundation</td>\
-                    <td>Earthquake manuscripts</td>\
-                    <td></td>\
-                    <td>“Rajab: Earthquake in dhuha, it’s a sign that wind, sea water and waves are very hard in that year”</td>\
-                  </tr>\
-                  <tr>\
-                    <td>Book of Sultan, Pahlawan, Hakim</td>\
-                    <td>Van de Tuuk manuscripts</td>\
-                    <td>Tuesday, 12th February 1861 (28th Jumadil akhir 1253 H)</td>\
-                    <td>Tectonic earthquake in Singkil 1852 destroyed Dutch infrastructures</td>\
-                  </tr>\
-                  <tr>\
-                    <td>Book of Sultan, Pahlawan, Hakim</td>\
-                    <td>Dutch Document</td>\
-                    <td>Friday, 29th September 1837 (1st Sya\'ban 1277 H)</td>\
-                    <td>Earthquake 7.3 SR in Sultan Muhammad Syah period (1824-1838)</td>\
-                  </tr>\
-                  <tr>\
-                    <td>Hermansyah: Aceh National Museum</td>\
-                    <td>Earthquake manuscript<br />07-01676</td>\
-                    <td>Monday, 24th November 1833 M (12th Rajab 1249H), at 10.20 Indonesian Time</td>\
-                    <td>“Rajab; Earthquake in dhuha time, it’s a sign that sea water will be hard tides”</td>\
-                  </tr>\
-                  <tr>\
-                    <td>Hermansyah: Aceh National Museum</td>\
-                    <td>Earthquake manuscripts 07_00841</td>\
-                    <td>Monday 24 November 1833 M (12th Rajab 1249H), at 10.20 Indonesian Times</td>\
-                    <td>“Rajab; Earthquake in dhuha time its a sign that the sea will be very hard”</td>\
-                  </tr>\
-                  <tr>\
-                    <td>Dayah Tanoh Abee, Aceh Besar</td>\
-                    <td>Earthquake manuscripts</td>\
-                    <td>Thursday, 3th November 1832M (9th Jumadil akhir, 1248 H)</td>\
-                    <td>"wa kanat al-zalzalah al-shadidah al-tsaniyah fajr yaum al-khamis tis\'at ayyam min jumada al-akhir sanah1248 min hijriah al-nabawiyah…” (2nd Earthquake has occurred in Thursday, l 9th Jumadil Akhir)”</td>\
-                  </tr>\
-                  <tr>\
-                    <td>Hermansyah: Surau Lubuk Ipuh Padang</td>\
-                    <td>Teks, Colofon</td>\
-                    <td>Friday, 10th February 1797 M (12th Sya\'ban, 1211 H)</td>\
-                    <td>“Rajab: Earthquake in dhuha time, it’s a sign that the sea will be very hard”</td>\
-                  </tr>\
-                  <tr>\
-                    <td>Wang Ta Yuan in Mc Kinnon (1988) in Meilianda (2009)</td>\
-                    <td>Lambri Archeological Manuscripts</td>\
-                    <td>1349 AD (around 600-700 years ago)</td>\
-                    <td>"This place is the most important trade center in Nan-wu-li (Aceh), big mountains like waves dash against it.”</td>\
-                  </tr>\
-                  <tr>\
-                    <td>Akhbar al-Sin wa’l Hind refer Rammi in Mc Kinnon (1988) in Meilianda (2009)</td>\
-                    <td>Lambri Archeological Manuscripts</td>\
-                    <td>1000 BP (around 9 Century)</td>\
-                    <td>"The island is washed by two seas . Harkand & that of Salahit" (Bay of Bengal and the Malacca Strait)</td>\
-                  </tr>\
-                  <tr>\
-                    <td>Leiden University</td>\
-                    <td>Doc. No. Or. 12.234 Supplement Catalogues by van Ronke</td>\
-                    <td></td>\
-                    <td>On chapter mention about earthquake treatise.</td>\
-                  </tr>\
-                </tbody>\
-              </table>\
-            '.trim()
+            entities: ["List of Manuscripts related earthquake and tsunami Aceh"]
           },
           {
             name: "Prose of Earthquake in Aceh, Sjeh Rih Krueng Raya, 1964",
-            content: '\
-              <a href="images/pdf/Geumpa_di_Acheh-ACEH_03142.pdf" class="pdf-button" target="_blank">View PDF</a>\
-              <img src="images/pdf/Geumpa_di_Acheh-ACEH_03142_01.jpg" class="pdf" />\
-              <img src="images/pdf/Geumpa_di_Acheh-ACEH_03142_02.jpg" class="pdf" />\
-              <img src="images/pdf/Geumpa_di_Acheh-ACEH_03142_03.jpg" class="pdf" />\
-              <img src="images/pdf/Geumpa_di_Acheh-ACEH_03142_04.jpg" class="pdf" />\
-              <img src="images/pdf/Geumpa_di_Acheh-ACEH_03142_05.jpg" class="pdf" />\
-              <img src="images/pdf/Geumpa_di_Acheh-ACEH_03142_06.jpg" class="pdf" />\
-              <img src="images/pdf/Geumpa_di_Acheh-ACEH_03142_07.jpg" class="pdf" />\
-              <img src="images/pdf/Geumpa_di_Acheh-ACEH_03142_08.jpg" class="pdf" />\
-              <img src="images/pdf/Geumpa_di_Acheh-ACEH_03142_09.jpg" class="pdf" />\
-              <img src="images/pdf/Geumpa_di_Acheh-ACEH_03142_10.jpg" class="pdf" />\
-              <img src="images/pdf/Geumpa_di_Acheh-ACEH_03142_11.jpg" class="pdf" />\
-              <img src="images/pdf/Geumpa_di_Acheh-ACEH_03142_12.jpg" class="pdf" />\
-              <img src="images/pdf/Geumpa_di_Acheh-ACEH_03142_13.jpg" class="pdf" />\
-              <img src="images/pdf/Geumpa_di_Acheh-ACEH_03142_14.jpg" class="pdf" />\
-              <img src="images/pdf/Geumpa_di_Acheh-ACEH_03142_15.jpg" class="pdf" />\
-              <img src="images/pdf/Geumpa_di_Acheh-ACEH_03142_16.jpg" class="pdf" />\
-              <img src="images/pdf/Geumpa_di_Acheh-ACEH_03142_17.jpg" class="pdf" />\
-              <img src="images/pdf/Geumpa_di_Acheh-ACEH_03142_18.jpg" class="pdf" />\
-              <img src="images/pdf/Geumpa_di_Acheh-ACEH_03142_19.jpg" class="pdf" />\
-              <img src="images/pdf/Geumpa_di_Acheh-ACEH_03142_20.jpg" class="pdf" />\
-              <img src="images/pdf/Geumpa_di_Acheh-ACEH_03142_21.jpg" class="pdf" />\
-            '.trim()
+            entities: ["Prose of Earthquake in Aceh, Sjeh Rih Krueng Raya, 1964"]
           }
         ]
       },
